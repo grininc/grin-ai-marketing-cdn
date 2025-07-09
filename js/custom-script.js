fab10fe5907a09466df0449659ea184fcab9c5e6
Gabe Eatmon <gabe.eatmon@grin.co>
2025-07-09 15:32:48 -0700

    Modals

---
+++ js/custom-script.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function ($) {
+  // Open modal
+  $('[class^="open-modal-"]').on("click", function () {
+    var classList = $(this).attr("class").split(/\s+/);
+    var modalNumber;
+
+    classList.forEach(function (className) {
+      var match = className.match(/^open-modal-(\d+)$/);
+      if (match) {
+        modalNumber = match[1];
+      }
+    });
+
+    if (modalNumber) {
+      $("#modal-" + modalNumber).addClass("active");
+    }
+  });
+
+  // Close modal
+  $(".modal .close").on("click", function () {
+    $(this).closest(".modal").removeClass("active");
+  });
+});
